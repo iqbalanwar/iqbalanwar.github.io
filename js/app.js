d0173f08ca3c7b510e6471075d89368dbd394e5d
Iqbal Anwar <iqbalaanwar@gmail.com>
2019-10-27 18:40:11 -0400

    Created dummy modals for each of the projects

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
 // if header can't be seen (height: 0vh?)
 // make navbar position fixed
 
+/* =============== TYPE 'Skills' TITLE: =============== */
 const blink = () => {
-
     const cursorLine = document.querySelector('.cursorLine').style;
     cursorLine.marginLeft = '1%';
     cursorLine.width = '10px';
@@ -15,8 +15,7 @@ const blink = () => {
     }
     setTimeout(blink, 500);
 };
-
-const ttyl = () => {
+const typed = () => {
     let skillsTitle = `  Skills`;
     let i = 0;
     const typing = () => {
@@ -28,11 +27,32 @@ const ttyl = () => {
             i++;
             setTimeout(typeTheString, 150);
         } else { 
-            // document.querySelector('.skillsTitle').innerHTML = "Skills";
             return blink(); 
         }
     }
     return typeTheString();
 };
+typed();
 
-ttyl();
+
+/* =============== CLICK MODALS IN PROJECTS SECTION: =============== */
+
+var modal = document.querySelector(".modal");
+var trigger = document.querySelectorAll(".projectBox");
+var closeButton = document.querySelector(".close-button");
+
+function toggleModal() {
+    modal.classList.toggle("show-modal");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+for(let i=0; i < trigger.length; i++) {
+    trigger[i].addEventListener("click", toggleModal);
+}
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
